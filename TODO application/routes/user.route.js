2c60dd12761d0a98140aeb8886e2912abd16eb5f
Tusharjoshi8 <tusharjoshi2385@gmail.com>
2026-04-14 00:13:31 +0530

    Delete TODO application directory

--- TODO application/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-import userSignupMidware from "../middleware/users/user.signupmidware.js"
-import usersignup from "../service/user.signupservice.js"
-
-const router = express.Router()
-router.post('/signup',userSignupMidware,usersignup)
-
-
-export default router
